af19a1a96911925491b71afc4a63037430c28686
Neo <neo.hung@quantatw.com>
2024-03-18 16:26:43 +0800

    test

--- assets/neojs/neosearch.js
+++ assets/neojs/neosearch.js
@@ -90,9 +90,8 @@ function buildResultList(results) {
         // Expected output: "Wed, 14 Jun 2017 07:00:00 GMT"
         searchitems = searchitems + '<time>' + new Date(results[itemnum].item.date).toUTCString().substring(4, 16) + '</time>';
         searchitems = searchitems + '</li>';
-        searchitems = searchitems + '<li>';
-        searchitems = searchitems + '<blockquote>' + results[itemnum].item.contents + '</blockquote>';
-        searchitems = searchitems + '</li>';
+        searchitems = searchitems + '<br>';
+        searchitems = searchitems + '<blockquote><em>' + results[itemnum].item.contents + '</em></blockquote>';
         
         //const contents = '<div class="prose px-4">' + results[itemnum].item.contents + '</div>';
     }
