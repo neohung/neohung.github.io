5066fdf2ecaae3e53499f42cbbaa97ff64e29cd6
Neo <neo.hung@quantatw.com>
2024-03-18 09:59:51 +0800

    test

--- assets/neojs/neosearch.js
+++ assets/neojs/neosearch.js
@@ -1,7 +1,35 @@
 
+var neofuse;
 var searchfirstRun = true;
 
 if (searchfirstRun) {
-    window.alert("neo searchfirstRun");
-    searchfirstRun = false;
+  loadNeoSearch();
+  searchfirstRun = false;
+}
+
+function fetchJSONFile(path, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          var data = JSON.parse(httpRequest.responseText);
+          if (callback) callback(data);
+        }
+      }
+    };
+  httpRequest.open('GET', path);
+  httpRequest.send();
+}
+
+function loadNeoSearch() {
+    // in head
+    const lang = document.querySelector(' > meta[name="lang"]')?.getAttribute?.('content')
+    // or in html
+    //const lang = document.documentElement.lang
+    // like "/zh-tw/index.json", if $lang exist use /xxx/index.json else /index.json 
+    console.log("lang head"); 
+    console.log(lang); 
+    const lang2 = document.documentElement.lang
+    console.log("lang html"); 
+    console.log(lang2); 
 }
